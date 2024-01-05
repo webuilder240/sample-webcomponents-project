af8235974778e5e89be581f0c6098f09affe6f45
Takuya Nishio <littlecub240@gmail.com>
2024-01-06 01:41:11 +0900

    multi page configure

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import react from '@vitejs/plugin-react'
 import preact from "@preact/preset-vite";
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), preact(), svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        sync_counter: resolve(__dirname, 'sync-counter.html'),
+      },
+    },
+  },
 })
